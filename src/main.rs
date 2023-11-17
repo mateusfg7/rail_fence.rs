10e6e6be4dbb13b5d7dc3cc1b680b619b99e8232
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-11-17 19:01:19 +0000

    refactor: create cipher mod with encode method

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
+mod cipher;
 mod cli;
-mod encode;
 
 use clap::Parser;
 use cli::CLI;
-use encode::encode;
 
 fn main() {
     let args = CLI::parse();
 
-    let encoded = encode(args.message.clone(), args.trails);
+    let encoded = cipher::encode(args.message.clone(), args.trails);
 
     println!("secret : {0}\nencoded: {encoded}", args.message)
 }
