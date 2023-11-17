8ebc1e10e37a455795e2cc43a635b2fd98850d27
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-11-17 14:10:58 +0000

    refactor: move `encode` function to separated `encode.rs` file

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,16 @@
-fn encode(secret: String, trails_number: usize) -> String {
-    let mut trails: Vec<Vec<char>> = vec![];
-    let mut i = 0;
-    loop {
-        if i < trails_number {
-            trails.push(vec![]);
-            i += 1;
-        } else {
-            break;
-        }
-    }
-
-    let mut curr_trail = 0;
-    for letter in secret.chars() {
-        trails[curr_trail].push(letter);
-        curr_trail = if curr_trail < trails_number - 1 {
-            curr_trail + 1
-        } else {
-            0
-        };
-    }
-
-    let mut encoded_secret = String::new();
-
-    trails.iter().for_each(|trail| {
-        trail.iter().for_each(|c: &char| {
-            encoded_secret.push(*c);
-        });
-    });
-
-    encoded_secret.to_uppercase()
-}
+mod encode;
+use encode::encode;
 
 fn main() {
     let secret = String::from("Mateus Felipe");
     let trails_number = 2;
 
-    let encoded = encode(secret, trails_number);
+    let encoded = encode(secret.clone(), trails_number);
 
-    println!("{}", encoded)
+    println!(
+        "
+    secret : {secret}
+    encoded: {encoded}
+    "
+    )
 }
